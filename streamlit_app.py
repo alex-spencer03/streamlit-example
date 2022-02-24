1af1e7f92767b73313e2e0a630386dbe5e09fb65
alex-spencer03 <65711433+alex-spencer03@users.noreply.github.com>
2022-02-24 16:09:34 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import numpy as np
 
 
 
@@ -27,8 +28,12 @@ data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text('Loading data...done!')
 
-st.subheader('Raw data')
-st.write(data)
+st.subheader('Number of pickups by hour')
+
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+
+st.bar_chart(hist_values)
 
 
 
