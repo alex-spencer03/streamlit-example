12e18b6d5075b1031898ff608591f496d443a2fe
alex-spencer03 <65711433+alex-spencer03@users.noreply.github.com>
2022-02-25 08:31:31 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,16 @@ with st.form("my_form"):
 st.write("Outside the form")
 
 
+st.line_chart({"data": [1, 5, 2, 6, 2, 1]})
 
+with st.expander("See explanation"):
+     st.write("""
+         The chart above shows some numbers I picked for you.
+         I rolled actual dice for these, so they're *guaranteed* to
+         be random.
+     """)
+     st.image("https://static.streamlit.io/examples/dice.jpg")
+    
 
 
 add_selectbox = st.sidebar.selectbox('How would you like to be contacted', ('Email','Home phone','Mobile phone'))
