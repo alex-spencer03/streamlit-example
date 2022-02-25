7dcce4b4d1326648fb4cbe37f69db3d2fd0c6da8
alex-spencer03 <65711433+alex-spencer03@users.noreply.github.com>
2022-02-25 13:25:19 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,7 +84,7 @@ with st.expander('Procurement and Tendering Options'):
 
 
 with st.form(key='columns_in_form'):
-    cols = st.beta_columns(5)
+    cols = st.columns(5)
     for i, col in enumerate(cols):
         col.selectbox(f'Make a Selection', ['click', 'or click'], key=i)
     submitted = st.form_submit_button('Submit')
