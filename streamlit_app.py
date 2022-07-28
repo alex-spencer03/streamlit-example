1f233b8312ceb1178acad7f59f614625bf7e6311
alex-spencer03 <65711433+alex-spencer03@users.noreply.github.com>
2022-07-28 16:59:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,11 +17,14 @@ col1, col2 = st.columns((1,1))
 
 with col1:
   st.selectbox('What is the location?', ('London', 'Nottingham', 'Edinburgh'))
+  st.selectbox('What is the tendering strategy?', ('Single', 'Double'))
+  st.selectbox('What BREEAM rating would you like?', ('Oustanding', 'Very Good'))
 
 
 with col2:
   st.selectbox('What is the base date?', ('Q2 2022', 'Q3 2022'))
-  
+  st.selectbox('What is the contract type?', ('Option1', 'Option2'))
+  st.selectbox('What WELL rating would you like?')
   
 st.sidebar.markdown('# Project and Preferences')
 
